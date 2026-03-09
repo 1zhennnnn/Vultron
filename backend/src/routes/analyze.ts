bae13c94a5591544fff50466dd22346b8489b661
1zhennnnn <zhennnnn0.0@gmail.com>
2026-03-09 21:58:57 +0800

    Remove orphaned v2 files causing TS build errors

--- backend/src/routes/analyze.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { analyzeContractHandler } from '../controllers/analyzeController';
-
-const router = Router();
-
-router.post('/analyze', analyzeContractHandler);
-
-export default router;
